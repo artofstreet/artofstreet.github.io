bfdf4cd36847db045c5d2f217d21241228afbba8
artofstreet <didgetup@gmail.com>
2026-02-15 11:28:02 +0900

    Update sw-sender.js

--- sw-sender.js
+++ sw-sender.js
@@ -1,19 +1,25 @@
-const CACHE_NAME = 'safezone-sender-v7'; // [중요] v6를 v7으로 바꿨습니다!
+const CACHE_NAME = 'safezone-sender-v7';
 const urlsToCache = [
-  'index.html',
-  'manifest-sender.json'
+  '/',
+  '/index.html',
+  '/manifest-sender.json',
+  '/icons/sender-192.png',
+  '/icons/sender-512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // 즉시 활성화
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim()); // 기존 페이지 즉시 제어
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
