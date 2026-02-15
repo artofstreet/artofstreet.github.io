ab2d6a8ccbd89afc1e5045a97d33376fc09266b3
artofstreet <didgetup@gmail.com>
2026-02-15 11:28:38 +0900

    Update sw-receiver.js

--- sw-receiver.js
+++ sw-receiver.js
@@ -1,19 +1,25 @@
-const CACHE_NAME = 'safezone-receiver-v6';
+const CACHE_NAME = 'safezone-receiver-v7';
 const urlsToCache = [
-  'receiver.html',
-  'manifest-receiver.json'
+  '/',
+  '/receiver.html',
+  '/manifest-receiver.json',
+  '/icons/receiver-192.png',
+  '/icons/receiver-512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
